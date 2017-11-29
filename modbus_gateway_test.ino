221a28e2da3307cdb8a6aee361ad848b5edcfe39
frankdunn <frankdunn37@gmail.com>
2017-11-29 22:10:19 +0200

    Update modbus_gateway_test.ino

--- modbus_gateway_test.ino
+++ modbus_gateway_test.ino
@@ -6,11 +6,40 @@ const char* ssid = "frank1";
 const char* password = "annarieannarie";
 
 // Start a TCP Server on port 5045
+
 WiFiServer server(502);
 WiFiClient client ;
 
 size_t len = 50;
 uint8_t sbuf[50];
+uint8_t rtu_buf[50];
+uint8_t rtu_len;
+
+uint16_t calcCRC(uint8_t u8_buff_size)
+{
+  uint32_t tmp, tmp2, flag;
+  tmp = 0xFFFF;
+
+  for (uint8_t i = 0; i < u8_buff_size; i++)
+  {
+    tmp = tmp ^ rtu_buf[i];
+
+    for (uint8_t j = 1; j <= 8; j++)
+    {
+      flag = tmp & 0x0001;
+      tmp >>= 1;
+
+      if (flag)
+        tmp ^= 0xA001;
+    }
+  }
+
+  tmp2 = tmp >> 8;
+  tmp = (tmp << 8) | tmp2;
+  tmp &= 0xFFFF;
+
+  return tmp;
+}
 
 void setup() {
   Serial.begin(115200);
@@ -48,12 +77,27 @@ void loop() {
       server.write(sbuf,len);
       // Echo input on Serial monitor
       //Serial.write(sbuf,len);
-      for (int i=0 ;i<readnum;i++){   //print received char in hex
+      for (int i=0 ;i<readnum;i++){
       Serial.print(sbuf[i],HEX);
       }
       Serial.print("\n\r");
-      Serial.println(readnum2);
-    }
+      rtu_len = sbuf[5];
+      Serial.printf("rtu msg len %d \n\r" ,sbuf[5]);
+      for (int i=0 ;i<readnum-6;i++){
+        rtu_buf[i] = sbuf[i+6];
+      }
+      for (int i=0 ;i<rtu_len;i++){
+      Serial.print(rtu_buf[i],HEX);
+      }
+      Serial.print("\n\r");
+      int value = calcCRC(rtu_len); 
+      Serial.println(value,HEX);
+      rtu_buf[rtu_len]  = (value & 0xFF00) >> 8;
+      rtu_buf[rtu_len+1] = value & 0x00FF;
+      for (int i=0 ;i<rtu_len+2;i++){
+      Serial.print(rtu_buf[i],HEX);
+      }
+      Serial.println();
   }
  }
-
+}
